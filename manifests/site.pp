57212c846fbcf713c35b21d9c8df3e26193c2b04
Lehlogonolo N. Masubelele <codesenju@gmail.com>
2020-05-30 07:04:14 +0200

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,11 +1,15 @@
 node default {
-file {'/root/README':
-  ensure => file,
-  content => 'This is a readme',
-  owner => 'root',
-  }
- package { 'docker':
-  ensure => installed,
-  name   => $docker,
-  }
+#file {'/root/README':
+#  ensure => file,
+#  content => 'This is a readme',
+#  owner => 'root',
+# }
+# package { 'docker':
+#  ensure => installed,
+#  name   => $docker,
+#  }
 }
+
+node 'master.puppet.vm' {
+  include role::master_server
+  }
