6cad5b73e5f83a515040abfd8ffd7d02e7bc9d0f
Lehlogonolo N. Masubelele <codesenju@gmail.com>
2020-05-30 15:14:56 +0200

    Create ssh_server.pp

---
+++ site/profile/manifests/ssh_server.pp
@@ -0,0 +1,15 @@
+class profile::ssh_serrver {
+        package {'openssh-server':
+                ensure => present,
+         }
+         service { 'sshd':
+                    ensure => 'running',
+                    enable => 'true',
+          }
+          ssh_authorizes_key { 'root@master.puppet.vm':
+                    ensure => present,
+                    user => 'root',
+                    type => 'ssh-rsa',
+                    key => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQC73Uh5bfSB7hMfy2BzsKugFEExdZ1qj8RMAWbwzEJcKp3hiWuWCKUXIST+VyO+PDcbzMW/K+4XbbL+yyxJGtfXDwRWqFLTmbUlT2KWTobSbcqkDQeWjbtzpc9pprcBuvEmqeY1HjwE/+JeoYqK9cNru5YRLrT1QpKUnyN86G9ZfZc1scKAIunmeI+trbxI0dMFkWDtjAQ+m4bccpwIU/fuQgDs0Oim3BNjKz/7VbXyWV4mdpDj2oPwucoQV5VWMkQQagm6b/brL/Ga/BRE6JPTMnnpGHPHbiRSqWWz2ImBwavPYdoNCPgdL39H+mF99MqWQpJgjYhOdLHlnuaH8tYV',
+          }
+}
